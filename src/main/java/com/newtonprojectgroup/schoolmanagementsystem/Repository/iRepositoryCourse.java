d91cc433df73e4b441fd22597a53a710c2da84a8
johanlind1984 <54557482+johanlind1984@users.noreply.github.com>
2020-03-26 21:44:57 +0100

    Fixade adminview så att admin kan lägga till/ta bort kurser i program

--- src/main/java/com/newtonprojectgroup/schoolmanagementsystem/Repository/iRepositoryCourse.java
+++ src/main/java/com/newtonprojectgroup/schoolmanagementsystem/Repository/iRepositoryCourse.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface iRepositoryCourse extends JpaRepository<Course, String> {
+public interface iRepositoryCourse extends JpaRepository<Course, Integer> {
 }
