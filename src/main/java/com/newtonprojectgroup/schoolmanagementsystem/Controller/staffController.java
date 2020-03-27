171183d952129ad9b6169a8a7ec401fa4c11b441
sighBRG <55457027+sighBRG@users.noreply.github.com>
2020-03-27 15:05:28 +0100

    Removed programController, added code to staffController & faculty-view.html.

--- src/main/java/com/newtonprojectgroup/schoolmanagementsystem/Controller/staffController.java
+++ src/main/java/com/newtonprojectgroup/schoolmanagementsystem/Controller/staffController.java
@@ -1,16 +1,30 @@
 package com.newtonprojectgroup.schoolmanagementsystem.Controller;
 
-import com.newtonprojectgroup.schoolmanagementsystem.Entity.Credentials;
-import com.newtonprojectgroup.schoolmanagementsystem.Entity.Person;
+import com.newtonprojectgroup.schoolmanagementsystem.Entity.*;
+import com.newtonprojectgroup.schoolmanagementsystem.Repository.iRepositoryPersonType;
+import com.newtonprojectgroup.schoolmanagementsystem.Repository.iRepositoryProgram;
+import com.newtonprojectgroup.schoolmanagementsystem.Repository.iRepositoryStudent;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class staffController {
 
+    private PersonType personType;
     private Person person;
     public Credentials credentials;
+
+    @Autowired
+    private iRepositoryPersonType repositoryPersonType;
+
+    @Autowired
+    private iRepositoryStudent repositoryStudent;
+    @Autowired
+    private iRepositoryProgram repositoryProgram;
 
     public staffController() {
     }
@@ -18,8 +32,21 @@ public class staffController {
     @RequestMapping("/staffview")
     public String staffView(Model theModel) {
 
+        List<PersonType> personTypes = repositoryPersonType.findAll();
+        List<Student> students = repositoryStudent.findAll();
+        List<Program> programs = repositoryProgram.findAll();
+
+        for (PersonType person : personTypes) {
+            if(person.getPersonTypeId() == 4) {
+                theModel.addAttribute("students", students);
+                theModel.addAttribute("programs", programs);
+            }
+        }
+
+
 
         theModel.addAttribute("Person", person);
+
         return "faculty-view";
     }
 
