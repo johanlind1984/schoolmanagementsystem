a49a5d56bc43a5a9b4ed9aa5c529f9ec303f1148
sighBRG <55457027+sighBRG@users.noreply.github.com>
2020-03-27 15:22:13 +0100

    Update Staff.java

--- src/main/java/com/newtonprojectgroup/schoolmanagementsystem/Entity/Staff.java
+++ src/main/java/com/newtonprojectgroup/schoolmanagementsystem/Entity/Staff.java
@@ -7,18 +7,6 @@ import javax.persistence.*;
 @PrimaryKeyJoinColumn(name="idstaff")
 public class Staff extends Person {
 
-    @Id
-    @Column(name="idstaff")
-    private String staffId;
-
     public Staff() {
-    }
-
-    public String getStaffId() {
-        return staffId;
-    }
-
-    public void setStaffId(String staffId) {
-        this.staffId = staffId;
     }
 }
