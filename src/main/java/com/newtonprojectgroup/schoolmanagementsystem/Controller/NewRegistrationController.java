f6c72a41c0b9d2dbdd61affa4eebb5f06e566b9b
Johanna Axnér <johanna.k.axner@gmail.com>
2020-03-31 13:33:41 +0200

    Update new RegistrationController and accountRequest html and java
    Added some form to html file
    Got the connection with database to work
    Removed nullable=false

--- src/main/java/com/newtonprojectgroup/schoolmanagementsystem/Controller/NewRegistrationController.java
+++ src/main/java/com/newtonprojectgroup/schoolmanagementsystem/Controller/NewRegistrationController.java
@@ -1,34 +1,46 @@
 package com.newtonprojectgroup.schoolmanagementsystem.Controller;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.newtonprojectgroup.schoolmanagementsystem.Entity.AccountRequest;
+import com.newtonprojectgroup.schoolmanagementsystem.Entity.PersonType;
 import com.newtonprojectgroup.schoolmanagementsystem.Repository.iRepositoryAccountRequests;
+import com.newtonprojectgroup.schoolmanagementsystem.Repository.iRepositoryPersonType;
 
 @Controller
 public class NewRegistrationController {
 	
 	@Autowired
 	iRepositoryAccountRequests accReqRepo;
-
+	@Autowired
+	iRepositoryPersonType persTypeRepo;
+	
 	@GetMapping("/new")
-	public String displayRegistrationForm(Model model) {
+	public String displayRegistrationForm (Model model) {
+		AccountRequest accountRequest =new AccountRequest();
 		
-		AccountRequest anAccountRequest=new AccountRequest();
+		List<PersonType>personTypeList=persTypeRepo.findAll();
 		
-		model.addAttribute("accountrequest", anAccountRequest);
+		model.addAttribute("accountrequest", accountRequest);
+		model.addAttribute("allPersonTypes", personTypeList);
+		
 		
 		return "accountrequest";
+		
 	}
+	@PostMapping ("/new")
+	public String createAccountRequest(@ModelAttribute("personTypeId") AccountRequest accountRequest, Model model) {
+		
+
+		
+		accReqRepo.save(accountRequest);
+		return "redirect:/new";
 	
-	@PostMapping ("/save")
-	public String createAccountRequest(AccountRequest accountRequest, Model model) {
-		 accReqRepo.save(accountRequest);
-		 
-		 return "redirect:/new";
-	}
+}
 }
