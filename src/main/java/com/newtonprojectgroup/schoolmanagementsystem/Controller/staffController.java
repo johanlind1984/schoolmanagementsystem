8889cb1cca33d0912b549b2fed3c4316150369e2
sighBRG <55457027+sighBRG@users.noreply.github.com>
2020-04-01 19:52:26 +0200

    Experimenting & Failing

--- src/main/java/com/newtonprojectgroup/schoolmanagementsystem/Controller/staffController.java
+++ src/main/java/com/newtonprojectgroup/schoolmanagementsystem/Controller/staffController.java
@@ -5,10 +5,12 @@ import com.newtonprojectgroup.schoolmanagementsystem.Entity.Person;
 import com.newtonprojectgroup.schoolmanagementsystem.Entity.Program;
 import com.newtonprojectgroup.schoolmanagementsystem.Entity.Student;
 import com.newtonprojectgroup.schoolmanagementsystem.Repository.iRepositoryProgram;
+import com.newtonprojectgroup.schoolmanagementsystem.Repository.iRepositoryStudent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -17,6 +19,10 @@ public class staffController {
 
     private Person person;
     public Credentials credentials;
+    private List<Program> programList;
+
+    @Autowired
+    private iRepositoryStudent repositoryStudent;
 
     @Autowired
     private iRepositoryProgram repositoryProgram;
@@ -24,33 +30,65 @@ public class staffController {
     public staffController() {
     }
 
+//    @RequestMapping("/staffview")
+//    public String staffView(Model theModel) {
+//
+//        List<Program> programs = repositoryProgram.findAll();
+//
+//
+//        theModel.addAttribute("programs", programs);
+//        theModel.addAttribute("Person", person);
+//
+//        return "faculty-view";
+//    }
+
+
+    //Skapa en knapp för att visa studenter i ett program
     @RequestMapping("/staffview")
-    public String staffView(Model theModel) {
+    public String chosenProgram(
+            @RequestParam(value = "chosen", required = false) Integer chosen, Model theModel) {
+
+        if (chosen == null) {
+            chosen = 1;
+        }
+
+
+
 
         List<Program> programs = repositoryProgram.findAll();
+        List<Student> students = repositoryProgram.findById(chosen).orElse(null).getStudentList();
 
-        theModel.addAttribute("programs", programs);
-        List<Student> students = repositoryProgram.findById(1).orElse(null).getStudentList();
-        theModel.addAttribute("students", students);
+            theModel.addAttribute("programs", programs);
+            theModel.addAttribute("students", students);
+            theModel.addAttribute("Person", person);
+
+//            theModel.addAttribute("chosen", chosen);
+//            List<Integer> chosenProgramID = Arrays.asList(1,2,3,4);
+//            theModel.addAttribute("chosenProgramID", chosenProgramID);
 
 
-        theModel.addAttribute("Person", person);
 
         return "faculty-view";
     }
 
-    //Skapa en knapp för att visa studenter i ett program
-    @RequestMapping("/chosenprogram")
-    public String chosenProgram(int chosen, Model theModel) {
-
-        List<Program> programs = repositoryProgram.findAll();
-        repositoryProgram.findById(chosen);
-        theModel.addAttribute("programs", programs);
-
-
-
-    return "faculty-view";
-    }
+//    @RequestMapping("/staffview")
+//    public String chosenProgram(
+//            @RequestParam( value = "chosen", required = true) int chosen, Model theModel) {
+//
+//        if(chosen == 0) {
+//            System.out.println(chosen);
+//            List<Student> students = repositoryProgram.findById(chosen).orElse(null).getStudentList();
+//
+//            programList = repositoryProgram.findAll();
+//            List<Program> programs = repositoryProgram.findAll();
+//
+//            theModel.addAttribute("programs", programs);
+//            theModel.addAttribute("students", students);
+//            System.out.println(chosen);
+//        }
+//
+//        return "faculty-view";
+//    }
 
 
     public void setUser(Person person) {
@@ -59,5 +97,6 @@ public class staffController {
 
     public void setCredentials(Credentials credentials) {
         this.credentials = credentials;
+
     }
 }
