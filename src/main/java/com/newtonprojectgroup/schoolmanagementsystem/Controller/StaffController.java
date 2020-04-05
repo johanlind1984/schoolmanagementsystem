2eed070e3c9eab817f7410133fbbf65b62b0f1c1
sighBRG <55457027+sighBRG@users.noreply.github.com>
2020-04-05 15:56:07 +0200

    Updating staffController and Faculty-view
    Added possibilty to change user information within page.

--- src/main/java/com/newtonprojectgroup/schoolmanagementsystem/Controller/StaffController.java
+++ src/main/java/com/newtonprojectgroup/schoolmanagementsystem/Controller/StaffController.java
@@ -1,6 +1,9 @@
 package com.newtonprojectgroup.schoolmanagementsystem.Controller;
 
-import com.newtonprojectgroup.schoolmanagementsystem.Entity.*;
+import com.newtonprojectgroup.schoolmanagementsystem.Entity.Credentials;
+import com.newtonprojectgroup.schoolmanagementsystem.Entity.Person;
+import com.newtonprojectgroup.schoolmanagementsystem.Entity.Program;
+import com.newtonprojectgroup.schoolmanagementsystem.Entity.Student;
 import com.newtonprojectgroup.schoolmanagementsystem.Repository.iRepositoryPerson;
 import com.newtonprojectgroup.schoolmanagementsystem.Repository.iRepositoryProgram;
 import com.newtonprojectgroup.schoolmanagementsystem.Repository.iRepositoryStaff;
@@ -8,7 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.security.Principal;
 import java.util.List;
@@ -30,6 +35,8 @@ public class StaffController {
     @Autowired
     private iRepositoryProgram repositoryProgram;
 
+    private Principal principal;
+
     public StaffController() {
     }
 
@@ -47,14 +54,39 @@ public class StaffController {
         return "faculty-view";
     }
 
+
+
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    public ModelAndView updateInformation(Principal principal,
+            @RequestParam("firstName") String firstName,
+            @RequestParam("lastName") String lastName,
+            @RequestParam("adress") String adress,
+            @RequestParam("email") String email) {
+
+        Person personFromDB = repositoryPerson.findById(principal.getName()).orElse(null);
+        assert personFromDB != null;
+        personFromDB.setFirstName(firstName);
+        personFromDB.setLastName(lastName);
+        personFromDB.setAdress(adress);
+        personFromDB.setEmail(email);
+
+        repositoryPerson.save(personFromDB);
+
+        return new ModelAndView("redirect:/staff/");
+    }
+
+
     @RequestMapping("/chosenprogram")
-    public String chosenProgram(@RequestParam("program") int chosenProgramId, Principal principal, Model theModel) {
+    public String chosenProgram(Principal principal, Model theModel,
+            @RequestParam("program") int chosenProgramId) {
 
         Person person = repositoryPerson.findById(principal.getName()).orElse(null);
         List<Program> programs = repositoryProgram.findAll();
         Program program = repositoryProgram.findById(chosenProgramId).orElse(null);
+        assert program != null;
         List<Student> studentList = program.getStudentList();
 
+        theModel.addAttribute("programTitle", program.getProgramName());
         theModel.addAttribute("programs", programs);
         theModel.addAttribute("chosenProgram", program);
         theModel.addAttribute("studentList", studentList);
