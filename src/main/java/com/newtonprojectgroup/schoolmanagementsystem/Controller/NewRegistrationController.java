727045b55234f068a43609766becbd2387c87798
Johanna Axnér <johanna.k.axner@gmail.com>
2020-04-06 08:17:13 +0200

    Update validering

--- src/main/java/com/newtonprojectgroup/schoolmanagementsystem/Controller/NewRegistrationController.java
+++ src/main/java/com/newtonprojectgroup/schoolmanagementsystem/Controller/NewRegistrationController.java
@@ -1,25 +1,20 @@
 package com.newtonprojectgroup.schoolmanagementsystem.Controller;
 import java.util.List;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.newtonprojectgroup.schoolmanagementsystem.Entity.AccountRequest;
-import com.newtonprojectgroup.schoolmanagementsystem.Entity.Credentials;
 import com.newtonprojectgroup.schoolmanagementsystem.Entity.PersonType;
 import com.newtonprojectgroup.schoolmanagementsystem.Repository.iRepositoryAccountRequests;
 import com.newtonprojectgroup.schoolmanagementsystem.Repository.iRepositoryCredentials;
 import com.newtonprojectgroup.schoolmanagementsystem.Repository.iRepositoryPersonType;
-import com.newtonprojectgroup.schoolmanagementsystem.Service.SaveAccountRequestValidator;
 
 @Controller
 @RequestMapping("/register")
@@ -55,18 +50,48 @@ public class NewRegistrationController {
 	@PostMapping ("/save")
 	public String createAccountRequest(@ModelAttribute("accountrequest") AccountRequest accountRequest, Model model) {
 		
-		Credentials credentials=credRepo.findById(accountRequest.getUserName()).orElse(null);
-		if (!credentials==null) {
+		if (credRepo.findById(accountRequest.getUserName()).orElse(null) !=  null || 
+				accReqRepo.findById(accountRequest.getUserName()).orElse(null) != null) {
+			System.out.println("Användarnamn upptaget");
+//			
+			List<PersonType>personTypeList=persTypeRepo.findAll();
+			
+			model.addAttribute("accountrequest", accountRequest);
+			model.addAttribute("allPersonTypes", personTypeList);
+			
 			return "accountRequest";
+			
 		}else {
 			CharSequence seq = java.nio.CharBuffer.wrap(accountRequest.getPassword());
 			accountRequest.setPassword(passwordEncoder.encode(seq).toCharArray());
-			
 			
 			accReqRepo.save(accountRequest);
 			
 		}
 		return "welcome-new-account";
+//		Credentials credentials = credRepo.findById(accountRequest.getUserName()).orElse(null); 		 		
+//		if (credentials.getUserName().equals(accountRequest.getUserName())) { 
+//			
+//			
+//			
+//			List<PersonType>personTypeList=persTypeRepo.findAll();
+//			
+//			
+//			
+//			model.addAttribute("accountrequest", accountRequest);
+//			model.addAttribute("allPersonTypes", personTypeList);
+//			
+//		
+//			return "accountRequest";
+//		}else {
+//			CharSequence seq = java.nio.CharBuffer.wrap(accountRequest.getPassword());
+//			accountRequest.setPassword(passwordEncoder.encode(seq).toCharArray());
+//			
+//			
+//			accReqRepo.save(accountRequest);
+//			
+//		}
+//		return "welcome-new-account";
 
 		
 	
