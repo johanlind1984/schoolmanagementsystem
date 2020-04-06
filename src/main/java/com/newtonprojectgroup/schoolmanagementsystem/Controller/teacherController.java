8d3299d107752634048800ee40d1268fb7c9cc26
annazamarou <annazamarou@gmail.com>
2020-04-06 19:01:42 +0200

    edit sida kommer inte up

--- src/main/java/com/newtonprojectgroup/schoolmanagementsystem/Controller/teacherController.java
+++ src/main/java/com/newtonprojectgroup/schoolmanagementsystem/Controller/teacherController.java
@@ -1,6 +1,9 @@
 package com.newtonprojectgroup.schoolmanagementsystem.Controller;
 
 
+import java.security.Principal;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,28 +12,17 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.newtonprojectgroup.schoolmanagementsystem.Entity.Person;
+import com.newtonprojectgroup.schoolmanagementsystem.Entity.Program;
+import com.newtonprojectgroup.schoolmanagementsystem.Entity.Student;
 import com.newtonprojectgroup.schoolmanagementsystem.Entity.Teacher;
-import com.newtonprojectgroup.schoolmanagementsystem.Repository.iRepositoryPerson;
-import com.newtonprojectgroup.schoolmanagementsystem.Repository.iRepositoryTeacher;
-import com.newtonprojectgroup.schoolmanagementsystem.Service.TeacherService;
-
-import com.newtonprojectgroup.schoolmanagementsystem.Entity.*;
 import com.newtonprojectgroup.schoolmanagementsystem.Repository.iRepositoryPerson;
 import com.newtonprojectgroup.schoolmanagementsystem.Repository.iRepositoryProgram;
 import com.newtonprojectgroup.schoolmanagementsystem.Repository.iRepositoryTeacher;
 import com.newtonprojectgroup.schoolmanagementsystem.Service.TeacherService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
-
-import java.security.Principal;
-import java.util.List;
 
 @Controller
 @RequestMapping("/teacher")
@@ -64,26 +56,26 @@ public class teacherController {
 		return "teacher";
 	}
 	
-	@RequestMapping("/edit/{idteacher}") 	
+	/*@RequestMapping("/edit/{idteacher}") 	
 	public ModelAndView editTeacher(@PathVariable(name = "idteacher") String idteacher) {
 		ModelAndView mav = new ModelAndView("edit_teacher"); 		
 		Teacher teacher = service.get(idteacher); 		
 		mav.addObject("teacher", teacher);
 		return mav; 	
-		}
+		}*/
 
 	@GetMapping("/edit")
-	public String editTeacher(Model model) {
+	public String editTeacher(Principal principal, Model model) {
 		System.out.println("submit");
-		Teacher teacher = repoteacher.findById("nils").orElse(null);
-		model.addAttribute("teacher", teacher);
+		Person person = repoperson.findById(principal.getName()).orElse(null);
+		model.addAttribute("Person", person);
 		
 		return "edit_teacher";
 	}
 	
 
 
-@RequestMapping(value = "/edit_teacher/submit", method = RequestMethod.POST)
+@RequestMapping(value = "/submit", method = RequestMethod.POST)
 public String saveTeacher(@ModelAttribute("teacher") Teacher teacher) {
 	service.save(teacher);
 	
--- src/main/java/com/newtonprojectgroup/schoolmanagementsystem/Service/TeacherService.java
+++ src/main/java/com/newtonprojectgroup/schoolmanagementsystem/Service/TeacherService.java
@@ -1,7 +1,5 @@
 package com.newtonprojectgroup.schoolmanagementsystem.Service;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
